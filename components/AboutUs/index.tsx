f98791b8bb657a738c8332ae2f01f52ac3a3b5b2
Olena Hrynykha <olenagrynyha@gmail.com>
2024-01-17 22:07:21 +0200

    add text and icons

--- components/AboutUs/index.tsx
+++ components/AboutUs/index.tsx
@@ -2,22 +2,67 @@
 import React from "react";
 import Button from "../UI/Button";
 import { Fade } from "react-awesome-reveal";
+import Car from "../Icons/Car";
+import Walk from "../Icons/Walk";
+
+interface IPlace {
+    title: string;
+    desc: { number: number; icon: React.ReactNode }[] | string;
+}
+
 const AboutUs = () => {
     const advantages = [
         {
-            number: "500+",
-            title: "Projects",
+            number: "7",
+            title: "Секцій",
             desc: "Over 500 lexury villas for“Home Away From Home” experience",
         },
         {
-            number: "40+",
-            title: "Locations",
+            number: "4",
+            title: "Поверховість",
             desc: "luxury villas and private holiday homes, from all across",
         },
         {
-            number: "24/7",
-            title: "Help",
+            number: "118",
+            title: "Квартир",
             desc: "24/7 Help service for all customers to guide and support",
+        },
+        {
+            number: "65",
+            title: "Паркінг",
+            desc: "24/7 Help service for all customers to guide and support",
+        },
+    ];
+    const places: IPlace[] = [
+        {
+            title: "Парк Замарстинівський ",
+            desc: "Безпосередньо біля будинку",
+        },
+        {
+            title: "ТЦ Спартак",
+            desc: [
+                { number: 12, icon: <Walk /> },
+                { number: 5, icon: <Car /> },
+            ],
+        },
+        {
+            title: "Школа №81, 97",
+            desc: [
+                { number: 12, icon: <Walk /> },
+                { number: 5, icon: <Car /> },
+            ],
+        },
+        {
+            title: "Лікарні Святого Пантелеймона та Миколая",
+            desc: [
+                { number: 7, icon: <Walk /> },
+                { number: 2, icon: <Car /> },
+            ],
+        },
+
+        {
+            title: "Гіпермаркет “Епіцентр”",
+            desc: [{ number: 13, icon: <Walk /> }],
         },
     ];
     return (
@@ -26,47 +71,86 @@ const AboutUs = () => {
             className=" bg-about-us-block bg-cover bg-center bg-no-repeat  relative"
         >
             <div className="w-full h-full bg-black bg-opacity-75 absolute"></div>
-            <div className="container flex flex-col md:flex-row gap-20 items-center justify-between py-[137px]">
+            <div className="container flex flex-col lg:flex-row gap-20 items-center justify-between pt-[137px]">
                 <div className="flex flex-col gap-4 px-3 md:px-0 max-w-[666px] relative z-20 justify-center items-center md:items-start text-center md:text-left">
                     <Fade direction="left">
                         <div className="text-sky-500 text-[32px] font-semibold">
-                            About us
+                            Про нас
                         </div>
                     </Fade>
                     <Fade direction="left" delay={300}>
                         <div className=" text-neutral-50 text-base font-normal mb-">
-                            Homeverse.io is a gated community with a great
-                            location. Located downtown, you’re within walking
-                            distance of Parks, and the best shopping, dining and
-                            entertainment Getting around is a breeze, with easy
-                            access to freeways, buses and trolleys. . Laundry is
-                            available on premises.
+                            SUMMERSTONE VILLAGE – це новий проєкт
+                            малоповерхового житла комфорт-класу, натхненний
+                            природою та сучасним ритмом життя. Знаходиться на
+                            вул. Орлика у Львові. Розташування дає змогу бути
+                            собою і обирати те, що любиш. Вийти з під&apos;їзду
+                            і опинитись посеред зеленого оазису лісопарку
+                            Замарстинівський, або пройтись до спортзалу чи на
+                            шопінг. Сісти за кермо та за 15 хвилин пити каву на
+                            Площі Ринок. Інфраструктура дозволяє якісно та
+                            ефективно планувати свій час, адже поруч - дитячі
+                            садки, школи, медичні установи, торгові центри.
                         </div>
                     </Fade>
-                    <Fade direction="left" delay={600}>
+                    {/* <Fade direction="left" delay={600}>
                         <Button
                             title="Читати більше"
                             className=" mt-9 md:mt-0"
                         />
-                    </Fade>
+                    </Fade> */}
                 </div>
-                <div className="flex flex-col relative z-20 gap-12">
-                    {advantages.map((item, index) => (
-                        <Fade direction="up" key={index}>
-                            <div className="flex flex-col justify-center items-center text-center">
-                                <div className="text-center text-sky-500 text-opacity-80 text-[32px] font-semibold ">
-                                    {item.number}
-                                </div>
-                                <div className="text-center text-neutral-50 text-opacity-60 text-base font-semibold">
-                                    {item.title}
-                                </div>
-                                <div className="w-[276px] text-center text-neutral-50 text-opacity-50 text-base font-normal">
+                <div className="flex flex-col relative z-20 gap-32">
+                    <div className="grid grid-cols-2 gap-16">
+                        {advantages.map((item, index) => (
+                            <Fade direction="up" key={index}>
+                                <div className="flex flex-col justify-center items-center text-center">
+                                    <div className="text-center text-sky-500 text-opacity-80 text-[48px] font-semibold ">
+                                        {item.number}
+                                    </div>
+                                    <div className="text-center text-neutral-50 text-opacity-60 text-lg font-semibold">
+                                        {item.title}
+                                    </div>
+                                    {/* <div className="w-[276px] text-center text-neutral-50 text-opacity-50 text-base font-normal">
                                     {item.desc}
+                                </div> */}
                                 </div>
-                            </div>
-                        </Fade>
-                    ))}
+                            </Fade>
+                        ))}
+                    </div>{" "}
                 </div>
+            </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-11 gap-x-2 container mt-24 pb-[137px] px-3">
+                {places.map((place, index) => (
+                    <Fade direction="up" key={index}>
+                        <div className="flex flex-col">
+                            <div className=" text-sky-500 text-opacity-80 text-[16px] font-semibold ">
+                                {place.title}
+                            </div>
+                            <div className="text-neutral-50 text-opacity-60 text-xs mt-4 ">
+                                {Array.isArray(place.desc) ? (
+                                    <div className="flex gap-6">
+                                        {place.desc.map((item) => (
+                                            <div
+                                                className="flex gap-4 items-center"
+                                                key={item.number}
+                                            >
+                                                {" "}
+                                                <div>{item.icon}</div>
+                                                <div>{item.number}хв</div>
+                                            </div>
+                                        ))}
+                                    </div>
+                                ) : (
+                                    place.desc
+                                )}
+                            </div>
+                            {/* <div className="w-[276px] text-center text-neutral-50 text-opacity-50 text-base font-normal">
+                                    {item.desc}
+                                </div> */}
+                        </div>
+                    </Fade>
+                ))}
             </div>
         </div>
     );
